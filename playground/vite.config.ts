aea5b21a1b2323461a4038d914d473f9d37c641a
elonehoo <hi@elonehoo.me>
2025-10-28 17:29:55 +0800

    refactor: update eslint fix

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import { PluginStart } from 'vite-plugin'
-import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
